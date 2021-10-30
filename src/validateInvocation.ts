32838f20bae2c25c465cb3ebd9a9f79bab9af0ab
Adam Williams <adam.williams@jupiterone.io>
2021-10-30 13:19:24 -0400

    Log clientId, tenantId

--- src/validateInvocation.ts
+++ src/validateInvocation.ts
@@ -3,13 +3,22 @@ import {
   IntegrationValidationError,
 } from '@jupiterone/integration-sdk-core';
 
-import { IntegrationConfig } from './types';
 import { GraphClient } from './ms-graph/client';
+import { IntegrationConfig } from './types';
 
 export function validateExecutionConfig(
   executionContext: IntegrationExecutionContext<IntegrationConfig>,
 ): void {
   const { clientId, clientSecret, tenant } = executionContext.instance.config;
+
+  executionContext.logger.info(
+    {
+      clientId,
+      tenantId: tenant,
+    },
+    'Configured to make Microsoft Graph API calls to tenantId acting as clientId',
+  );
+
   if (!clientId || !clientSecret || !tenant) {
     throw new IntegrationValidationError(
       'Config requires all of {clientId, clientSecret, tenant}',
